67bc89fbb770036ecceaa0dd8adb5de7238c7a85
s.nakano <s.nakano@guppy.co.jp>
2019-05-10 11:03:31 +0900

    ignore filter process if dd_trace_php is not installed

--- Routing/Filter/DatadogFilter.php
+++ Routing/Filter/DatadogFilter.php
@@ -1,52 +1,51 @@
 <?php
 App::uses('DispatcherFilter', 'Routing');
 
-use DDTrace\Tag;
-use DDTrace\Type;
-use DDTrace\GlobalTracer;
-use DDTrace\Util\TryCatchFinally;
-
 class DatadogFilter extends DispatcherFilter
 {
     public function beforeDispatch(CakeEvent $event)
     {
-		if ($this->invokeRequestSpan($event)) {
-			$this->injectViewSpan();
-		}
+        if (!class_exists('DDTrace\\GlobalTracer')) {
+            return true;
+        }
+
+        if ($this->invokeRequestSpan($event)) {
+            $this->injectViewSpan();
+        }
 
         return true;
-	}
+    }
 
-	private function invokeRequestSpan(CakeEvent $event)
-	{
-        $scope = GlobalTracer::get()->getRootScope();
+    private function invokeRequestSpan(CakeEvent $event)
+    {
+        $scope = DDTrace\GlobalTracer::get()->getRootScope();
         if (null === $scope) {
             return false;
         }
         $appName = Configure::read('Datadog.appName');
         $request = $event->data['request'];
         $controller = $request->params['controller'];
-		$action = $request->params['action'];
+        $action = $request->params['action'];
 
         $span = $scope->getSpan();
         $span->overwriteOperationName('cakephp.request');
-        $span->setTag(Tag::SERVICE_NAME, $appName);
-        $span->setTag(Tag::RESOURCE_NAME, $controller . '/' . $action);
+        $span->setTag(DDTrace\Tag::SERVICE_NAME, $appName);
+        $span->setTag(DDTrace\Tag::RESOURCE_NAME, $controller . '/' . $action);
         $span->setTag('cakephp.controller', $controller);
         $span->setTag('cakephp.action', $action);
         $span->setTag('http.url', Router::url($request->here(), true));
 
-		return true;
-	}
+        return true;
+    }
 
-	private function injectViewSpan()
-	{
+    private function injectViewSpan()
+    {
         dd_trace('View', 'render', function () {
-			$args = func_get_args();
-            $scope = GlobalTracer::get()->startActiveSpan('cakephp.view');
+            $args = func_get_args();
+            $scope = DDTrace\GlobalTracer::get()->startActiveSpan('cakephp.view');
             $span = $scope->getSpan();
-            $span->setTag(Tag::SPAN_TYPE, Type::WEB_SERVLET);
-            return TryCatchFinally::executePublicMethod($scope, $this, 'render', $args);
+            $span->setTag(DDTrace\Tag::SPAN_TYPE, DDTrace\Type::WEB_SERVLET);
+            return DDTrace\Util\TryCatchFinally::executePublicMethod($scope, $this, 'render', $args);
         });
-	}
+    }
 }
